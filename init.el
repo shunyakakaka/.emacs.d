028ef039ab36ceacebb8e7364bb3600af685956d
s-katada <shunya.saitama@gmail.com>
2023-04-24 10:23:28 +0900

    feat: mermaidの設定を追加

--- init.el
+++ init.el
@@ -40,6 +40,32 @@
 ;; yes noで答えるのを y nにする
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; markdown-modeの設定
+(use-package markdown-mode
+  :mode ("\\.md\\'" . markdown-mode))
+
+;; mermaid-modeの設定
+(use-package mermaid-mode
+  :mode (("\\.mmd\\'" . mermaid-mode)
+	 ("\\.md\\'" . mermaid-mode))
+  :config
+  (setq mermaid-mode-map
+	(let ((map mermaid-mode-map))
+	  (define-key map (kbd "C-c C-c") nil)
+	  (define-key map (kbd "C-c C-f") nil)
+	  (define-key map (kbd "C-c C-b") nil)
+	  (define-key map (kbd "C-c C-r") nil)
+	  (define-key map (kbd "C-c C-o") nil)
+	  (define-key map (kbd "C-c C-d") nil)
+	  (define-key map (kbd "C-c C-d c") 'mermaid-compile)
+	  (define-key map (kbd "C-c C-d c") 'mermaid-compile)
+	  (define-key map (kbd "C-c C-d f") 'mermaid-compile-file)
+	  (define-key map (kbd "C-c C-d b") 'mermaid-compile-buffer)
+	  (define-key map (kbd "C-c C-d r") 'mermaid-compile-region)
+	  (define-key map (kbd "C-c C-d o") 'mermaid-open-browser)
+	  (define-key map (kbd "C-c C-d d") 'mermaid-open-doc)
+	  map)))
+
 (use-package fish-mode
   :ensure t
   :mode "\\.fish\\'"
